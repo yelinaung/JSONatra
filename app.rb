49afba437a3cf9a8e755301981f504cb8fe566c5
Ye Lin Aung <me@yelinaung.com>
2014-05-06 16:36:47 +0630

    Init commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+configure do
+  set :public_folder, File.dirname(__FILE__) + '/json'
+end
+
+get '/:name' do
+  content_type 'application/json'
+  if params[:name].nil? then 
+    File.readlines(params[:name])
+  else
+    "File not found"
+  end
+end
